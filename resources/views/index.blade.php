10ae5d2c6d7928b264cd7171fddad428e4527666
Robert Hu <rhu001@gmail.com>
2016-01-16 14:23:00 -0800

    Move css to its own file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,42 +3,9 @@
     <head>
         <title>Instasearch</title>
 
-<!--         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
- -->
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .imgElement {
-                padding: 10px;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-            }
-        </style>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/instasearch.css') }}">
     </head>
     <body>
         <div class="container">
