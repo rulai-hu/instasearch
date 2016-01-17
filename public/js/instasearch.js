18c0d9a30b0da8104fc1ace47d2846bac426122f
Rulai Hu <rhu001@gmail.com>
2016-01-17 12:24:08 -0800

    Add hover event handler and caption content

--- public/js/instasearch.js
+++ public/js/instasearch.js
@@ -1,19 +1,44 @@
 $( document ).ready(function() {
+  // Hide the loading gif asap
   $("#loadingSpinner").hide();
-  $( "#searchButton" ).on( "click", function() {
+
+  // Bind the AJAX call, the pretty transitions to the search button onclick
+  $( "#searchButton" ).on("click", function() {
+    // Do some clean up first..
     $(".img-element").remove();
     $(".errors").hide();
     $(".info").fadeOut(100, function() {
       $("#loadingSpinner").fadeIn(100);
     });
-    var val = $("#searchInput").val();
 
-    $.get("/search/" + encodeURI(val), function(response) {
+    var input = $("#searchInput").val();
+
+    $.get("/search/" + encodeURI(input), function(response) {
       $("#loadingSpinner").fadeOut(200);
 
       $.each(response[0].data, function(idx, element) {
-        var str = "<img class=\"img-element\" src=\"" + element.images.standard_resolution.url + "\">";
-        $("#imagesContainer").append(str).hide().fadeIn();
+        // Just doing a little more processing here to make the
+        // captions look neater.
+        var captionText = element.caption.text.length > 80 ? 
+          element.caption.text.substring(0, 80) + '...' :
+          element.caption.text;
+
+        // If the caption contains no spaces then we prevent an overflow by
+        // truncating it early.
+        captionText = (captionText.indexOf(' ') > 36 || captionText.indexOf(' ') == -1) ? 
+          captionText.substring(0, 50) + '...' :
+          captionText;
+
+        // Here we form the overlay that's hidden until a mousehover occurs
+        var text = '<div class="four columns username"><strong>'
+        + element.user.username + '</strong></div><div class="eight columns caption">'
+        + captionText + '</div>';
+
+        var imageElement = '<div class="img-container"><img class="img-element" src="' 
+          + element.images.standard_resolution.url
+          + '"><div class="img-text"><div class="row">' + text + '</div></div></div>';
+
+        $("#imagesContainer").append(imageElement).hide().fadeIn();
       });
 
     }).fail(function(response) {
@@ -23,6 +48,12 @@ $( document ).ready(function() {
           $(".errors").append("<p>" + element + "</p>");
         });
       });
+    }).done(function() {
+        $(".img-container").hover(function(){
+          $(this).find(".img-text").fadeIn(200);
+        }, function(){
+          $(this).find(".img-text").fadeOut(200);
+        });
     });
   });
 });
