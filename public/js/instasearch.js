ce226dcb74bf7fd7cb780f1188c7d251d9e6656e
Rulai Hu <rhu001@gmail.com>
2016-01-17 03:41:05 -0800

    Add fade ins and fade outs to transitions

--- public/js/instasearch.js
+++ public/js/instasearch.js
@@ -1,13 +1,28 @@
-$( document ).ready(function(){
+$( document ).ready(function() {
+  $("#loadingSpinner").hide();
   $( "#searchButton" ).on( "click", function() {
+    $(".img-element").remove();
+    $(".errors").hide();
+    $(".info").fadeOut(100, function() {
+      $("#loadingSpinner").fadeIn(100);
+    });
     var val = $("#searchInput").val();
 
-    $.get("/search/" + encodeURI(val), function(data) {
-      $.each(data[0].data, function(idx, element) {
+    $.get("/search/" + encodeURI(val), function(response) {
+      $("#loadingSpinner").fadeOut(200);
+
+      $.each(response[0].data, function(idx, element) {
         var str = "<img class=\"img-element\" src=\"" + element.images.standard_resolution.url + "\">";
-        $("#imagesContainer").append(str);
+        $("#imagesContainer").append(str).hide().fadeIn();
       });
 
+    }).fail(function(response) {
+      $("#loadingSpinner").fadeOut(200, function() {
+        $(".errors").empty().show();
+        $.each(response.responseJSON.input, function(idx, element) {
+          $(".errors").append("<p>" + element + "</p>");
+        });
+      });
     });
   });
 });
