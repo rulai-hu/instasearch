d1270e05dbe37d7f795635f4b35c31492f745867
Robert Hu <rhu001@gmail.com>
2016-01-17 00:04:39 -0800

    JS tweaks

--- public/js/instasearch.js
+++ public/js/instasearch.js
@@ -3,9 +3,10 @@ $( document ).ready(function(){
     var val = $("#searchInput").val();
 
     $.get("/search/" + encodeURI(val), function(data) {
-        $.each(data[0].data, function(idx, element) {
-            $(".results").append("<img class=\"imgElement\" src=" + element.images.thumbnail.url + ">")
-        });
+      $.each(data[0].data, function(idx, element) {
+        var str = "<img class=\"img-element\" src=\"" + element.images.standard_resolution.url + "\">";
+        $("#imagesContainer").append(str);
+      });
 
     });
   });
