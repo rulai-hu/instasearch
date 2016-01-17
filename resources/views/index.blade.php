6ab92517600a92e8da80cdfb5b77656191f554ec
Robert Hu <rhu001@gmail.com>
2016-01-16 17:27:19 -0800

    Clean up main view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,38 +1,45 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Instasearch</title>
+<html lang="en">
+  <head>
+    <title>Instasearch</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/instasearch.css') }}">
-    </head>
-    <body>
-        <div class="container">
-            <div class="content" id="imagesBody">
-                <div class="title">InstaSearch</div>
-                <!-- <form action="demo_form.asp"> -->
-                    <input type="text" id="searchInput" name="fname"></br>
-                    <button id="searchButton">Search</button>
-                <!-- </form> -->
-                <div id="imagesDiv">
-                    <!-- <img src="https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/s150x150/e35/c135.0.810.810/12407467_1259312484095593_713811279_n.jpg"> -->
-                </div>
-            </div>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/normalize.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/skeleton.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/instasearch.css') }}">
+  </head>
+  <body>
+    <div class="banner">
+      <div class="container">
+        <div class="row">
+          <div class="offset-by-three six columns text-center title">
+            <h2>InstaSearch</h2>
+          </div>
         </div>
-        <script>
-            $( document ).ready(function(){
-                $( "#searchButton" ).on( "click", function() {
-                    var val = $("#searchInput").val();
 
-                    $.get( "/search/" + encodeURI(val), function( data ) {
-                        $.each(data[0].data, function(idx, element) {
-                            $("#imagesDiv").append("<img class=\"imgElement\" src=" + element.images.thumbnail.url + ">")
-                        });
+        <div class="row">
+          <div class="offset-by-three six columns search-input">
+            <input class="u-full-width" type="text" id="searchInput" placeholder="input type">
+          </div>
+        </div>
 
-                    });
-                });
-            });
-        </script>
-    </body>
+        <div class="row">
+          <div class="offset-by-four four columns search-button text-center">
+            <button class="button-primary u-full-width" id="searchButton">Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="results"></div>
+      </div>
+    </div>
+
+    <script src="{{ asset('js/instasearch.js') }}"></script>
+
+  </body>
 </html>
